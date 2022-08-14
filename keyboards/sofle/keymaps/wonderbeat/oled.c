0db074cb804a3af00cc91dbff97cde229d6666e0
denisg <denisg.santiment.net>
2022-08-14 22:59:25 +0300

    74 leds

--- keyboards/sofle/keymaps/wonderbeat/oled.c
+++ keyboards/sofle/keymaps/wonderbeat/oled.c
@@ -150,42 +150,40 @@
 
  static void render_logo(void) {
      static const char PROGMEM qmk_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0};
-
      oled_write_P(qmk_logo, false);
  }
 
  static void print_status_narrow(void) {
      /* this fixes the screen on and off bug */
-     if (current_wpm > 0) {
+     anim_sleep = timer_read32();
+     if (current_wpm > 1) {
          oled_on();
-         anim_sleep = timer_read32();
+         // Print current mode
+         oled_write_P(PSTR("\n\n"), false);
+
+         switch (get_highest_layer(layer_state)) {
+             case 0:
+                 oled_write_ln_P(PSTR("QWRT"), false);
+                 break;
+             case 1:
+                 oled_write_ln_P(PSTR("CDH"), false);
+                 break;
+             case 2:
+                 oled_write_P(PSTR("Raise"), false);
+                 break;
+             case 3:
+                 oled_write_P(PSTR("Lower"), false);
+                 break;
+             default:
+                 break;
+         }
+         oled_write_ln_P(PSTR("\n\n\nCPSLK"), led_usb_state.caps_lock);
+ #    ifdef LUNA
+         render_luna(0, 13);
+ #    endif
      } else if (is_oled_on() && timer_elapsed32(anim_sleep) > OLED_TIMEOUT) {
          oled_off();
-         return;
      }
-     // Print current mode
-     oled_write_P(PSTR("\n\n"), false);
-
-     switch (get_highest_layer(layer_state)) {
-         case 0:
-             oled_write_ln_P(PSTR("QWRT"), false);
-             break;
-         case 1:
-             oled_write_ln_P(PSTR("CDH"), false);
-             break;
-         case 2:
-             oled_write_P(PSTR("Raise"), false);
-             break;
-         case 3:
-             oled_write_P(PSTR("Lower"), false);
-             break;
-         default:
-             break;
-     }
-     oled_write_ln_P(PSTR("\n\n\nCPSLK"), led_usb_state.caps_lock);
- #    ifdef LUNA
-     render_luna(0, 13);
- #    endif
  }
 
  oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -199,12 +197,16 @@
      current_wpm   = get_current_wpm();
      led_usb_state = host_keyboard_led_state();
 
-     if (current_wpm > 70) {
+     if (current_wpm > 110) {
          rgb_matrix_sethsv(HSV_RED);
-     } else if (current_wpm > 35) {
+     } else if (current_wpm > 75) {
          rgb_matrix_sethsv(HSV_YELLOW);
-     } else if (current_wpm > 20) {
+     } else if (current_wpm > 55) {
+         rgb_matrix_sethsv(HSV_PURPLE);
+     } else if (current_wpm > 35) {
          rgb_matrix_sethsv(HSV_MAGENTA);
+     } else if (current_wpm > 23) {
+         rgb_matrix_sethsv(HSV_TEAL);
      } else if (current_wpm == 0) {
          rgb_matrix_sethsv(HSV_BLUE);
      }
--- keyboards/sofle/keymaps/wonderbeat/rules.mk
+++ keyboards/sofle/keymaps/wonderbeat/rules.mk
@@ -5,3 +5,7 @@ OLED_DRIVER = SSD1306
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 TAP_DANCE_ENABLE = yes
+
+# Additional thing to reduce compiled size
+LTO_ENABLE = yes
+NO_USB_STARTUP_CHECK = yes
